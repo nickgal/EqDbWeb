6f3d08bdbb0e0aead8155f6277b3eeb73da91184
Nick Gal <nickgal@me.com>
2023-08-19 11:17:17 -0700

    Add spell page

---
+++ components/DetailsWindow.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="details-window">
+    <slot />
+  </div>
+</template>
+<style>
+.details-window {
+  background-image: url('/ClassicChatBox.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: smaller;
+
+  height: 300px;
+  padding: 55px 100px;
+  box-sizing: border-box;
+  aspect-ratio: 925 / 388;
+}
+.details-window .item-icon {
+  float: right;
+  padding: 10px;
+  filter: drop-shadow(0 2px 5px #333);
+}
+</style>
---
+++ pages/spells/[id].vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Spell } from 'drizzle/schema';
+
+interface SpellResponse {
+  spell: Spell
+}
+
+const route = useRoute()
+const [id, ...rest] = route.params.id.toString().split(/-(.*)/s)
+const { data } = await useFetch<SpellResponse>(`/api/spells/${id}`)
+
+const hasSlug = !!rest.join('')
+const spellName = data?.value?.spell?.name
+if (!hasSlug && spellName) {
+  await navigateTo({
+    path: `${route.path}-${slugify(spellName)}`,
+    replace: true
+  })
+}
+
+useSeoMeta({
+  title: () => `${spellName} - Spells`
+})
+</script>
+<template>
+  <div v-if="data">
+    <DetailsWindow>
+      {{ data.spell.name }}
+    </DetailsWindow>
+    <hr />
+    <code>
+      {{ data.spell }}
+    </code>
+  </div>
+</template>
