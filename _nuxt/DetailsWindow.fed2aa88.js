ef4d3fe1b54f25f89d7ecc480244f9afeffccfa2
nickgal <nickgal@users.noreply.github.com>
2023-08-26 03:54:51 +0000

    deploy: 40a08db1bdd4491df5db6c055112e641ff7ad76f

--- _nuxt/DetailsWindow.fed2aa88.js
+++
@@ -1,1 +0,0 @@
-import{_,o as t,c as n,z as o}from"./entry.613514cf.js";const s={},c={class:"details-window"};function a(e,r){return t(),n("div",c,[o(e.$slots,"default")])}const d=_(s,[["render",a]]);export{d as _};
