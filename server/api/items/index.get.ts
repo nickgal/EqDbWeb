76feca4ba834f17ed437483acb5c34d708a133c7
Nick Gal <nickgal@me.com>
2023-08-27 17:07:19 -0700

    Add endpoint page/ids query example

--- server/api/items/index.get.ts
+++ server/api/items/index.get.ts
@@ -1,9 +1,22 @@
+import { and, inArray, SQL } from "drizzle-orm";
 import { items } from "~/drizzle/schema";
 import { db } from "~/server/sqlite-service";
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
+  const filters: SQL[] = []
+
+  const { id: ids, page } = getQuery(event)
+  const perPage = 250
+  const pageOffset = page ? (page as number - 1) * perPage : 0
+
+  if (ids) {
+    filters.push(inArray(items.id, ids))
+  }
+
   try {
-    const itemsResp = db.select().from(items).limit(250).all();
+    const itemsResp = db.select().from(items)
+      .where(and(...filters))
+      .limit(perPage).offset(pageOffset).all();
     return { "items" : itemsResp}
   } catch (e: any) {
     throw createError({
