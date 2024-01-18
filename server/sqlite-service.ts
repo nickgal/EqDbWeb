ca63c5698e66fe05817a95a6e03b8341853ffe37
Nick Gal <nickgal@me.com>
2024-01-18 10:57:47 -0800

    Update database to https://github.com/SecretsOTheP/EQMacEmu/commit/a7e78ce5b2d222d5f286030facc6dba8bb1ac915

--- server/sqlite-service.ts
+++ server/sqlite-service.ts
@@ -2,5 +2,5 @@ import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from '~/drizzle/schema';
 
-export const sqlite = new Database('quarm_2023-12-26-18_31.db');
+export const sqlite = new Database('quarm_2024-01-18-18_41.db');
 export const db: BetterSQLite3Database = drizzle(sqlite, { schema });
